61d8c5224e98a59b89c5678fdb1484e40a444e4b
yudenisov <yudenisov@mail.ru>
2016-10-01 09:07:37 +0300

    Second Commit

--- replace_pattern_at_file/My Project/AssemblyInfo.vb
+++ replace_pattern_at_file/My Project/AssemblyInfo.vb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
@@ -19,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-<Assembly: Guid("9906b9cb-3cdf-42db-9fc1-269bfc3bdf5b")>
+<Assembly: Guid("65f88f19-487b-4c8d-839b-7a166a654475")>
 
 ' Сведения о версии сборки состоят из следующих четырех значений:
 '
@@ -34,40 +33,3 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("1.0.0.0")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
-=======
-﻿Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
-
-' Общие сведения об этой сборке предоставляются следующим набором 
-' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
-' общие сведения об этой сборке.
-
-' Проверьте значения атрибутов сборки
-
-<Assembly: AssemblyTitle("replace_pattern_at_file")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("replace_pattern_at_file")>
-<Assembly: AssemblyCopyright("Copyright ©  2016")>
-<Assembly: AssemblyTrademark("")>
-
-<Assembly: ComVisible(False)>
-
-'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-<Assembly: Guid("9906b9cb-3cdf-42db-9fc1-269bfc3bdf5b")>
-
-' Сведения о версии сборки состоят из следующих четырех значений:
-'
-'      Основной номер версии
-'      Дополнительный номер версии 
-'   Номер сборки
-'      Редакция
-'
-' Можно задать все значения или принять номера сборки и редакции по умолчанию 
-' используя "*", как показано ниже:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
->>>>>>> replace_pattern_at_file/master
--- replace_pattern_at_file/MyLibrary001.vb
+++ replace_pattern_at_file/MyLibrary001.vb
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿Imports System
+Imports System
 Imports System.IO
 
 Module MyLibrary001
@@ -35,41 +34,3 @@ Module MyLibrary001
     End Sub
 
 End Module
-=======
-﻿Imports System
-Imports System.IO
-
-Module MyLibrary001
-
-    ' В следующем примере используется класс StreamReader для чтения содержимого текстового файла.
-    ' path -- полное или краткое имя файла
-    ' Возвращаемое значение -- прочитанная строка текста
-    Function ReadTextFile(ByVal path As String) As String
-        Dim sr As System.IO.StreamReader
-        Dim Contents As String
-        sr = New StreamReader(path)
-        Contents = sr.ReadToEnd()
-        sr.Close()
-        Return Contents
-    End Function
-
-    'Следующий пример совершает обратные действия, используя аналогичный подход: выполняет запись в текстовый файл с помощью класса StreamWriter.
-    ' path -- полное или краткое имя
-    ' value -- значение, записываемое в текстовый файл
-    Sub WriteTextFile(ByVal path As String, ByVal value As String)
-        Dim sr As StreamWriter
-        sr = New StreamWriter(path)
-        sr.Write(value)
-        sr.Close()
-    End Sub
-
-    ' Подпрограмма заменяет в файла, заданном как sFileName, строку sTemplate строкой sPattern
-    Sub Replace_In_File(ByVal sFileName As String, ByVal sTemplate As String, ByVal sPattern As String)
-        Dim sInitBuffer, sDestBuffer As String ' Начальный и конечный буфер, содержащий текстовый файл
-        sInitBuffer = ReadTextFile(sFileName)
-        sDestBuffer = sInitBuffer.Replace(sTemplate, sPattern)
-        WriteTextFile(sFileName, sDestBuffer)
-    End Sub
-
-End Module
->>>>>>> replace_pattern_at_file/master
